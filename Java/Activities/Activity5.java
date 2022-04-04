1ab1d061f9cb5dcd5c90c339b810d4bc582c3b2a
Anamikajoshi1989 <102527623+Anamikajoshi1989@users.noreply.github.com>
2022-04-04 11:04:01 +0530

    Add files via upload
    Activity 5 classes-Abstraction

---
+++ Java/Activities/Activity5.java
@@ -0,0 +1,13 @@
+package activities;
+
+public class Activity5 {
+	public static void main(String args[])
+	{
+		String title="The Ulchemist";
+		Book newNovel=new MyBook();
+		newNovel.getTitle();
+		newNovel.setTitle(title);
+		
+	}
+
+}
---
+++ Java/Activities/MyBook.java
@@ -0,0 +1,14 @@
+package activities;
+
+public class MyBook extends Book {
+
+	
+	@Override
+	public void setTitle(String str) {
+		// TODO Auto-generated method stub
+		title=str;
+	}
+	
+	
+
+}
